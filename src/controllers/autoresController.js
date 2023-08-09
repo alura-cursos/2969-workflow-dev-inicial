5315e1d716d350f432528a8bd5a10113cdf9593c
claudiozh <claudiorodrigozh@gmail.com>
2023-08-09 11:51:08 -0300

    add metodo para listar livros por autor

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -68,6 +68,18 @@ class AutoresController {
       return res.status(500).json(err.message);
     }
   };
+
+  static listarLivrosPorAutor = async (req, res) => {
+    const { params } = req;
+    try {
+      const listaLivros = await Autor.pegarLivrosPorAutor(params.id);
+      const autor = await Autor.pegarPeloId(params.id);
+      
+      return res.status(200).json({ autor, livros: listaLivros });
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  };
 }
 
 export default AutoresController;
