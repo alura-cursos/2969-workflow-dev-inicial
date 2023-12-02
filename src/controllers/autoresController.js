ed98f7586c1cc91ee32e1c8c5d5c4250450af8a4
Davi Coelho <davi.cmbarreto@gmail.com>
2023-12-02 17:11:37 -0400

    feat: :sparkles: add: métodos para listar livros por autor

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -68,6 +68,18 @@ class AutoresController {
       return res.status(500).json(err.message);
     }
   };
+
+  static listarLivrosPorAutor = async (req, res) => {
+    const { params } = req;
+
+    try {
+      const listarLivros = await Autor.pegaLivrosPorAutor(params.id);
+      const autor = await Autor.pegarPeloId(params.id);
+      return res.status(200).json({ autor, livros: listarLivros });
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  };
 }
 
 export default AutoresController;
