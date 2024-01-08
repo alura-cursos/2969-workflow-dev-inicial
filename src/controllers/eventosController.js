d2dff12961d01c7813336a526c79f5f2f39a54ed
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2024-01-08 15:34:43 -0300

    feat: :sparkles: Add: feature flag para /eventos e teste de /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,12 +1,18 @@
 import Evento from '../models/evento.js';
 
+const liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+
 class EventosController {
   static async listarEventos(req, res) {
-    try {
-      const eventoRecebido = await Evento.pegarEventos();
-      return res.status(200).json(eventoRecebido);
-    } catch (err) {
-      return res.status(500).json(err.message);
+    if (liberaAcessoEventos()) {
+      try {
+        const eventoRecebido = await Evento.pegarEventos();
+        return res.status(200).json(eventoRecebido);
+      } catch (err) {
+        return res.status(500).json(err.message);
+      }
+    } else {
+      return res.status(404).send();
     }
   }
 }
