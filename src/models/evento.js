29128d9623572b8f0a3263480f07de0dc9ce3dec
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-03-13 14:27:14 -0300

    feat: :sparkles: add: modelo eventos e teste do modelo

---
+++ src/models/evento.js
@@ -0,0 +1,28 @@
+import db from "../db/dbconfig.js";
+
+class Evento {
+  constructor({ id, nome, descricao, data, autor_id, created_at, updated_at }) {
+    this.id = id || null;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.data = data;
+    this.autor_id = autor_id;
+    this.created_at = created_at || new Date().toISOString();
+    this.updated_at = updated_at || new Date().toISOString();
+  }
+
+  static async pegarEventos() {
+    return [
+      {
+        id: 1,
+        nome: "lançamento",
+        descricao: "descricao",
+        data: "2024-03-14",
+        autor_id: 1,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+  }
+}
+export default Evento;
---
+++ src/test/models/evento.test.js
@@ -0,0 +1,17 @@
+import { describe, expect, it, jest } from "@jest/globals";
+import Evento from "../../models/evento.js";
+
+describe("Testando o modelo Evento", () => {
+  const objetoEvento = {
+    nome: "Evento teste",
+    descricao: "descricao evento teste",
+    data: "2024-01-01",
+    autor_id: 1,
+  };
+
+  it("Deve instanciar um novo Evento", () => {
+    const evento = new Evento(objetoEvento);
+
+    expect(evento).toEqual(expect.objectContaining(objetoEvento));
+  });
+});
