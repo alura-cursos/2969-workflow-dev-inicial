9c9f8dc9020ce6fdd2ff6c29a9923d2dfe5b095d
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-03-15 16:15:47 -0300

    add: teste para método autores x livros

--- populate.sql
+++ populate.sql
@@ -7,10 +7,10 @@ CREATE TABLE autores(
 );
 
 INSERT INTO autores (nome, nacionalidade) 
-VALUES  ("JRR Tolkien", "sul-africano"),
-        ("Ursula LeGuin", "estadunidense"),
-        ("Machado de Assis", "brasileira"),
-        ("Júlio Cortázar", "argentino");
+VALUES  ('JRR Tolkien', 'sul-africano'),
+        ('Ursula LeGuin', 'estadunidense'),
+        ('Machado de Assis', 'brasileira'),
+        ('Júlio Cortázar', 'argentino');
 
 CREATE TABLE editoras(
   id      INTEGER NOT NULL PRIMARY KEY,
@@ -22,10 +22,10 @@ CREATE TABLE editoras(
 );
 
 INSERT INTO editoras (nome, cidade, email) 
-VALUES  ("Europa-América", "Lisboa", "e@e.com"),
-        ("Morro Branco", "São Paulo", "m@m.com"),
-        ("Aleph", "São Paulo", "al@al.com"),
-        ("Ateliê", "São Paulo", "a@a.com");
+VALUES  ('Europa-América', 'Lisboa', 'e@e.com'),
+        ('Morro Branco', 'São Paulo', 'm@m.com'),
+        ('Aleph', 'São Paulo', 'al@al.com'),
+        ('Ateliê', 'São Paulo', 'a@a.com');
 
 CREATE TABLE livros(
   id          INTEGER NOT NULL PRIMARY KEY,
@@ -41,11 +41,11 @@ CREATE TABLE livros(
 
 INSERT INTO livros (titulo, paginas, autor_id, editora_id)
 VALUES 
-   ("O Hobbit", 230, 1, 1),
-   ("O Silmarillion", 400, 1, 1),
-   ("O Silmarillion", 400, 1, 1),
-   ("O Feiticeiro de Terramar", 450, 2, 2),
-   ("Os Despossuídos", 300, 2, 3),
-   ("Memórias Póstumas de Brás Cubas", 150, 3, 4);
+   ('O Hobbit', 230, 1, 1),
+   ('O Silmarillion', 400, 1, 1),
+   ('O Silmarillion', 400, 1, 1),
+   ('O Feiticeiro de Terramar', 450, 2, 2),
+   ('Os Despossuídos', 300, 2, 3),
+   ('Memórias Póstumas de Brás Cubas', 150, 3, 4);
   
 .exit
