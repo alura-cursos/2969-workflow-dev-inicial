2001b797f208b3c4069dbc11269f6afbfde6566e
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-03-20 15:16:57 -0300

    test: :white_check_mark: add: testes para o método livros x autor
    Teste unitário para a pesquisa de livros dado um autor.

--- populate.sql
+++ populate.sql
@@ -6,11 +6,11 @@ CREATE TABLE autores(
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO autores (nome, nacionalidade) 
-VALUES  ("JRR Tolkien", "sul-africano"),
-        ("Ursula LeGuin", "estadunidense"),
-        ("Machado de Assis", "brasileira"),
-        ("Júlio Cortázar", "argentino");
+INSERT INTO autores (nome, nacionalidade)
+VALUES  ('JRR Tolkien', 'sul-africano'),
+        ('Ursula LeGuin', 'estadunidense'),
+        ('Machado de Assis', 'brasileira'),
+        ('Júlio Cortázar', 'argentino');
 
 CREATE TABLE editoras(
   id      INTEGER NOT NULL PRIMARY KEY,
@@ -21,11 +21,11 @@ CREATE TABLE editoras(
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO editoras (nome, cidade, email) 
-VALUES  ("Europa-América", "Lisboa", "e@e.com"),
-        ("Morro Branco", "São Paulo", "m@m.com"),
-        ("Aleph", "São Paulo", "al@al.com"),
-        ("Ateliê", "São Paulo", "a@a.com");
+INSERT INTO editoras (nome, cidade, email)
+VALUES  ('Europa-América', 'Lisboa', 'e@e.com'),
+        ('Morro Branco', 'São Paulo', 'm@m.com'),
+        ('Aleph', 'São Paulo', 'al@al.com'),
+        ('Ateliê', 'São Paulo', 'a@a.com');
 
 CREATE TABLE livros(
   id          INTEGER NOT NULL PRIMARY KEY,
@@ -40,12 +40,12 @@ CREATE TABLE livros(
 );
 
 INSERT INTO livros (titulo, paginas, autor_id, editora_id)
-VALUES 
-   ("O Hobbit", 230, 1, 1),
-   ("O Silmarillion", 400, 1, 1),
-   ("O Silmarillion", 400, 1, 1),
-   ("O Feiticeiro de Terramar", 450, 2, 2),
-   ("Os Despossuídos", 300, 2, 3),
-   ("Memórias Póstumas de Brás Cubas", 150, 3, 4);
-  
+VALUES
+   ('O Hobbit', 230, 1, 1),
+   ('O Silmarillion', 400, 1, 1),
+   ('O Silmarillion', 400, 1, 1),
+   ('O Feiticeiro de Terramar', 450, 2, 2),
+   ('Os Despossuídos', 300, 2, 3),
+   ('Memórias Póstumas de Brás Cubas', 150, 3, 4);
+
 .exit
